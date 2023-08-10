761d3c5041cf952da2e1a96034ae7eb235310de6
Loksin <loksinlegis@gmail.com>
2023-08-10 20:38:27 +1000

    Some more comments

--- Work3.py
+++ Work3.py
@@ -4,12 +4,12 @@ import random
 import matplotlib.pyplot as plt
 # Device configuration
 device = torch.device('cpu')
-zs = torch.zeros((400,400))
+zs = torch.zeros((800,800))
 #start at center
 #use dimensions 800x and 800y with for loop of 6
 #or use dimensions 400x and 400y with for loop of 5
 zs[int(zs.size(dim=0)/2)][int(zs.size(dim=1)/2)] = 1
-for i in range(5):
+for i in range(6):
     up = torch.roll(zs,-1*(3**i),0)
     down = torch.roll(zs,1*(3**i),0)
     left = torch.roll(zs,-1*(3**i),1)
